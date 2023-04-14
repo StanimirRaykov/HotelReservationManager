b081ba6cc436fe9f1b0b5c3c70025d4143b3bc5c
StanimirRaykov <mirkokr2004@gmail.com>
2023-04-14 14:34:28 +0300

    Folder creations + Template

---
+++ HotelReservationManager/Services/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservationManager.Data;
+using HotelReservationManager.DTO.Requests;
+using HotelReservationManager.DTO.Responses;
+
+namespace HotelReservationManager.Services
+{
+    public interface ICategoryService
+    {
+        //TODO: Should we use DTOs here?
+        public Task<ProductResponseDTO> GetCategoryByIdAsync(int categoryId);
+        public Task<ICollection<ProductResponseDTO>> GetAllCategorysAsync();
+        public Task<ProductResponseDTO> CreateAsync(CategoryRequestDTO category);
+        public Task<ProductResponseDTO> UpdateAsync(CategoryRequestDTO category);
+        public Task<ProductResponseDTO> DeleteAsync(int categoryId);
+    }
+}
---
+++ HotelReservationManager/Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using HotelReservationManager.DTO.Requests;
+using HotelReservationManager.DTO.Responses;
+
+namespace HotelReservationManager.Services
+{
+    public interface IProductService
+    {
+        //TODO: Should we use DTOs here?
+        public Task<ProductResponseDTO> GetProductByIdAsync(int productId);
+        public Task<ICollection<ProductResponseDTO>> GetAllProductsAsync();
+        public Task<ProductResponseDTO> CreateAsync(ProductRequestsDTO item, string userId);
+        public Task<ProductResponseDTO> UpdateAsync(int productID, ProductRequestsDTO item, string userId);
+        public Task<ProductResponseDTO> DeleteAsync(int productId);
+    }
+}
